757a18d2dbff33615e1909acc48f1876bfdef868
ManaInfectedRP <52261763+ManaInfectedRP@users.noreply.github.com>
2025-05-01 04:14:43 +0200

    Chunk Dividing + Tree Generation

--- Assets/TerrainGeneration.cs
+++ Assets/TerrainGeneration.cs
@@ -1,38 +1,60 @@
 using System;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Serialization;
+using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    public int dirtLayerHeight = 5;
-    
+    [Header("Tile/Sprites")]
     public Sprite dirt;
     public Sprite grass;
     public Sprite stone;
-
     public Sprite log;
     public Sprite leaf;
-
+    
+    [Header("Tree Generation")]
     public int treeChance = 10;
+    public int minTreeHeight = 4;
+    public int maxTreeHeight = 6;
+    
+    [Header("Generation Settings")]
     public bool generateCaves = true;
-
-    public float surfaceValue = 0.25f;
+    public int chunkSize = 16;
     public int worldSize = 100;
-    public float caveFreq = 0.08f;
-    public float terrainFreq = 0.04f;
+    
+    public int dirtLayerHeight = 5;
+    public float surfaceValue = 0.25f;
     public float heightMultiplier = 25f;
     public int heightAddition = 25;
     
+    [Header("Noise Settings")]
+    public float caveFreq = 0.08f;
+    public float terrainFreq = 0.04f;
     public float seed;
     public Texture2D noiseTexture;
+
+    public GameObject[] worldChunks;
+    private List<Vector2> worldTiles = new List<Vector2>();
 
     private void Start()
     {
         seed = Random.Range(-10000, 10000);
         GenerateNoiseTexture();
+        CreateChunks();
         GenerateTerrain();
+    }
+
+    public void CreateChunks()
+    {
+        int numChunks = worldSize / chunkSize;
+        worldChunks = new GameObject[numChunks];
+        for (int i = 0; i < numChunks; i++)
+        {
+            GameObject newChunk = new GameObject();
+            newChunk.name = i.ToString();
+            newChunk.transform.parent = this.transform;
+            worldChunks[i] = newChunk;
+        }
     }
 
     public void GenerateTerrain()
@@ -46,14 +68,17 @@ public class TerrainGeneration : MonoBehaviour
                 Sprite tileSprite;
                 if (y < height - dirtLayerHeight)
                 {
+                    // *Are we below dirt level*?
                     tileSprite = stone;
                 }
                 else if (y < height - 1)
                 {
+                    // *Are we below grass level*?
                     tileSprite = dirt;
                 }
                 else
                 {
+                    // *Top layer of Terrain*
                     tileSprite = grass;
                 }
 
@@ -67,6 +92,19 @@ public class TerrainGeneration : MonoBehaviour
                 else
                 {
                     PlaceTile(tileSprite, x, y);
+                }
+
+                if (y >= height - 1)
+                {
+                    int t = Random.Range(0, treeChance);
+                    if (t == 1)
+                    {
+                        // *Generate Tree*
+                        if (worldTiles.Contains(new Vector2(x, y)))
+                        {
+                            GenerateTree(x, y + 1);
+                        }
+                    }
                 }
             }
         }
@@ -89,13 +127,44 @@ public class TerrainGeneration : MonoBehaviour
         noiseTexture.Apply();
     }
 
-    public void PlaceTile(Sprite tileSprite, float x, float y)
+    void GenerateTree(int x, int y)
+    {
+        // *How does the tree Look?*
+        int treeHeight = Random.Range(minTreeHeight, maxTreeHeight);
+
+        // *Log Generation*
+        for (int i = 0; i < treeHeight; i++)
+        {
+            PlaceTile(log, x, y +i);
+        }
+        
+        // *Generate Leaves*
+        PlaceTile(leaf,x,y + treeHeight);
+        PlaceTile(leaf,x,y + treeHeight +1);
+        PlaceTile(leaf,x,y + treeHeight +2);
+        
+        PlaceTile(leaf,x -1,y + treeHeight);
+        PlaceTile(leaf,x -1,y + treeHeight +1);
+        
+        PlaceTile(leaf,x +1,y + treeHeight);
+        PlaceTile(leaf,x +1,y + treeHeight +1);
+    }
+    
+    public void PlaceTile(Sprite tileSprite, int x, int y)
     {
         GameObject newTile = new GameObject();
-        newTile.transform.parent = this.transform;
+
+        float chunkCoord = (Mathf.Round(x / chunkSize) * chunkSize);
+        chunkCoord /= chunkSize;
+        Debug.Log(chunkCoord);
+        newTile.transform.parent = worldChunks[(int)chunkCoord].transform;
+        
+        
         newTile.AddComponent<SpriteRenderer>();
         newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
         newTile.name = tileSprite.name;
         newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+        
+        worldTiles.Add(newTile.transform.position - (Vector3.one * 0.5f));
     }
 }
